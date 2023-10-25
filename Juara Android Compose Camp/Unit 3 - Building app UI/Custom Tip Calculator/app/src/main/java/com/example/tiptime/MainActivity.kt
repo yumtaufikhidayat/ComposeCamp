0aec8ce09df7d0f1afbbdd041def76541bc8312a
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-10-25 17:02:48 +0700

    Implement custom text field.

--- Juara Android Compose Camp/Unit 3 - Building app UI/Custom Tip Calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Juara Android Compose Camp/Unit 3 - Building app UI/Custom Tip Calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -66,8 +67,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
+
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -83,8 +88,17 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
+            label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = { tipInput = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
@@ -99,6 +113,7 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -107,7 +122,7 @@ fun EditNumberField(
         value = value,
         label = {
             Text(
-                text = stringResource(id = R.string.bill_amount)
+                text = stringResource(label)
             )
         },
         singleLine = true,
